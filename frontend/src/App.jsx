4a99d5a5cdd1f82cf941d3cda765a6e5808eb5f2
Anuj More <moreofme7777@gmail.com>
2026-02-26 23:37:31 +0530

    Add Checkout page component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import HomePage from "./pages/home-page/HomePage";
 import { Route, Routes } from "react-router";
 import "./App.css";
+import Checkout from "./pages/checkout/Checkout";
 
 function App() {
     return (
         <Routes>
             <Route index element={<HomePage />} />
+            <Route path="/checkout" element={<Checkout />} />
         </Routes>
     );
 }
