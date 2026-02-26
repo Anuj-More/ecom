de65ef7b55bf0bb547594d41e96f036c1380d376
Anuj More <moreofme7777@gmail.com>
2026-02-26 23:45:10 +0530

    Add Orders page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import HomePage from "./pages/home-page/HomePage";
 import { Route, Routes } from "react-router";
 import "./App.css";
 import Checkout from "./pages/checkout/Checkout";
+import Orders from "./pages/orders/Orders";
 
 function App() {
     return (
         <Routes>
             <Route index element={<HomePage />} />
             <Route path="/checkout" element={<Checkout />} />
+            <Route path="/orders" element={<Orders />} />
         </Routes>
     );
 }
